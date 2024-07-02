a23073630a02a3d075c34b42c988abf5a466867f
dangviethung096 <dangviethung096@gmail.com>
2024-07-02 11:07:21 +0700

    update testing

--- testing.go
+++ testing.go
@@ -44,3 +44,27 @@ func TestAPI[T any](apiInfo TestApiInfo[T]) (HttpResponse, HttpError) {
 	// Call handler
 	return apiInfo.Handler(ctx, req)
 }
+
+func TestAPIWithContext[T any](ctx *HttpContext, apiInfo TestApiInfo[T]) (HttpResponse, HttpError) {
+	ctx.requestID = ID.GenerateID()
+	// Get url
+	ctx.URL = apiInfo.URL
+	ctx.Method = apiInfo.Method
+
+	ctx.urlParams = apiInfo.Queries
+	var req = apiInfo.Body.(T)
+
+	// Validate go struct with tag
+	errValidate := validate.StructCtx(ctx, req)
+	if errValidate != nil {
+		errMessage := "Request invalid: "
+		for _, err := range errValidate.(validator.ValidationErrors) {
+			errMessage = fmt.Sprintf("%s {Field: %s, Tag: %s, Value: %s}", errMessage, err.Field(), err.Tag(), err.Value())
+		}
+
+		return nil, NewHttpError(http.StatusBadRequest, ERROR_BAD_BODY_REQUEST, errMessage, nil)
+	}
+
+	// Call handler
+	return apiInfo.Handler(ctx, req)
+}
