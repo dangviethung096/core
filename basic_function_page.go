b16177097247fd0d1d5896bec1270fbd6dd3caca
Đặng Việt Hưng <dangviethung096@gmail.com>
2024-08-14 16:16:53 +0700

    update basic function

--- basic_function_page.go
+++ basic_function_page.go
@@ -10,6 +10,14 @@ func subtract(x int, y int) int {
 	return x - y
 }
 
+func multiply(x int, y int) int {
+	return x * y
+}
+
+func divide(x int, y int) int {
+	return x / y
+}
+
 func seq(start int, end int) []int {
 	result := make([]int, end-start+1)
 	for i := range result {
@@ -22,4 +30,6 @@ var basicFunctionMap = template.FuncMap{
 	"add":      add,
 	"subtract": subtract,
 	"seq":      seq,
+	"multiply": multiply,
+	"divide":   divide,
 }
