7c27e467a4c74fc82cda777af97fab14ccdcc8c0
Đặng Việt Hưng <dangviethung096@gmail.com>
2024-08-19 15:36:30 +0700

    update oracle query

--- oracle_db.go
+++ oracle_db.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"database/sql"
+	"fmt"
+	"reflect"
 
 	"github.com/lib/pq"
 )
@@ -11,14 +13,14 @@ type oracleSession struct {
 }
 
 func (session oracleSession) SaveDataToDB(ctx Context, data DataBaseObject) Error {
-	query, args, insertError := GetInsertQuery(data)
+	query, args, insertError := GetInsertQueryForOracle(data)
 	if insertError != nil {
 		ctx.LogError("Error when get insert data = %#v, err = %s", data, insertError.Error())
 		return insertError
 	}
 
 	ctx.LogInfo("Insert query = %v, args = %v", query, args)
-	if _, err := session.ExecContext(ctx, query, args...); err != nil {
+	if _, err := session.ExecContext(ctx, query, args); err != nil {
 		ctx.LogError("Error insert data = %#v, err = %v", data, err)
 		return ERROR_INSERT_TO_DB_FAIL
 	}
@@ -27,26 +29,15 @@ func (session oracleSession) SaveDataToDB(ctx Context, data DataBaseObject) Erro
 }
 
 func (session oracleSession) SaveDataToDBWithoutPrimaryKey(ctx Context, data DataBaseObject) Error {
-	query, args, pkAddress, insertError := GetInsertQueryWithoutPrimaryKey(data)
+	query, args, _, insertError := GetInsertQueryWithoutPrimaryKeyForOracle(data)
 	if insertError != nil {
 		ctx.LogError("Error when get insert data = %#v, err = %s", data, insertError.Error())
 		return insertError
 	}
 
 	ctx.LogInfo("Insert query = %v, args = %v", query, args)
-	row := session.QueryRowContext(ctx, query, args...)
-
-	err := row.Scan(pkAddress)
-	if err != nil {
+	if _, err := session.ExecContext(ctx, query, args); err != nil {
 		ctx.LogError("Error insert data = %#v, err = %v", data, err)
-		pqError, ok := err.(*pq.Error)
-		if ok {
-			if pqError.Code.Name() == "unique_violation" {
-				return ERROR_DB_UNIQUE_VIOLATION
-			} else if pqError.Code.Name() == "foreign_key_violation" {
-				return ERROR_DB_FOREIGN_KEY_VIOLATION
-			}
-		}
 		return ERROR_INSERT_TO_DB_FAIL
 	}
 
@@ -54,45 +45,337 @@ func (session oracleSession) SaveDataToDBWithoutPrimaryKey(ctx Context, data Dat
 }
 
 func (session oracleSession) DeleteDataInDB(ctx Context, data DataBaseObject) Error {
+	query, args, deleteError := GetDeleteQueryForOracle(data)
+	if deleteError != nil {
+		ctx.LogError("Error when get delete data = %#v, err = %s", data, deleteError.Error())
+		return deleteError
+	}
+
+	ctx.LogInfo("Delete query = %v, args = %v", query, args)
+	if _, err := session.ExecContext(ctx, query, args); err != nil {
+		ctx.LogError("Error delete data = %#v, err = %v", data, err)
+		return NewError(ERROR_CODE_FROM_DATABASE, err.Error())
+	}
+
 	return nil
 }
 
 func (session oracleSession) DeleteDataWithWhereQuery(ctx Context, data DataBaseObject, whereQuery string) Error {
+	query := fmt.Sprintf("DELETE FROM %s WHERE %s", data.GetTableName(), whereQuery)
+
+	ctx.LogInfo("Delete query = %v", query)
+	ret, err := session.ExecContext(ctx, query)
+	if err != nil {
+		ctx.LogError("Error delete data = %#v, err = %v", data, err)
+		pqError, ok := err.(*pq.Error)
+		if ok {
+			if pqError.Code.Name() == DB_ERROR_NAME_FOREIGN_KEY_VIOLATION {
+				return ERROR_DB_FOREIGN_KEY_VIOLATION
+			}
+		}
+		return NewError(ERROR_CODE_FROM_DATABASE, err.Error())
+	}
+
+	rowDeleted, err := ret.RowsAffected()
+	if err != nil {
+		ctx.LogError("Error get rows affected = %v", err)
+		return NewError(ERROR_CODE_FROM_DATABASE, err.Error())
+	}
+
+	if rowDeleted == 0 {
+		ctx.LogInfo("No row deleted in query: %s", query)
+		return nil
+	}
+
 	return nil
 }
 
 func (session oracleSession) UpdateDataInDB(ctx Context, data DataBaseObject) Error {
+	query, args, updateError := GetUpdateQueryForOracle(data)
+	if updateError != nil {
+		ctx.LogError("Error when get update data = %#v, err = %s", data, updateError.Error())
+		return updateError
+	}
+
+	ctx.LogInfo("Update query = %v, args = %v", query, args)
+	if _, err := session.ExecContext(ctx, query, args); err != nil {
+		ctx.LogError("Error update data = %#v, err = %s", data, err.Error())
+		return NewError(ERROR_CODE_FROM_DATABASE, err.Error())
+	}
+
 	return nil
 }
 
 func (session oracleSession) SelectById(ctx Context, data DataBaseObject) Error {
+	query, params, err := GetSelectQuery(data)
+	if err != nil {
+		ctx.LogError("Error when get data = %#v, err = %s", data, err.Error())
+		return err
+	}
+
+	primaryKeys, numPrimaryKeys := splitPrimaryKey(data)
+	if numPrimaryKeys == 0 {
+		ctx.LogError("Error not found primary key = %#v", data)
+		return ERROR_NOT_FOUND_PRIMARY_KEY
+	}
+
+	for i, key := range primaryKeys {
+		if i == 0 {
+			query += fmt.Sprintf(" WHERE %s = :%s", key, key)
+		} else {
+			query += fmt.Sprintf(" AND %s = :%s", key, key)
+		}
+	}
+
+	args, found := searchMapPrimaryKey(data)
+	if !found {
+		ctx.LogError("Error not found primary key = %#v", data)
+		return ERROR_DB_ERROR
+	}
+
+	ctx.LogInfo("Select query = %v, args = %v", query, args)
+
+	row := session.QueryRowContext(ctx, query, args)
+	if err := row.Scan(params...); err != nil {
+		ctx.LogError("Error select data = %#v, err = %v", data, err.Error())
+		if err == sql.ErrNoRows {
+			return ERROR_NOT_FOUND_IN_DB
+		}
+		return NewError(ERROR_CODE_FROM_DATABASE, err.Error())
+	}
+
 	return nil
 }
 
 func (session oracleSession) ListAllInTable(ctx Context, data DataBaseObject) (any, Error) {
-	return nil, nil
+	query, params, err := GetSelectQuery(data)
+	if err != nil {
+		ctx.LogError("Error when get update data = %#v, err = %s", data, err.Error())
+		return nil, err
+	}
+
+	ctx.LogInfo("Select query = %v", query)
+	rows, errQuery := session.QueryContext(ctx, query)
+	if errQuery != nil {
+		ctx.LogError("Error select table %s, err = %s", data.GetTableName(), errQuery.Error())
+		return nil, ERROR_DB_ERROR
+	}
+
+	// Get list of struct
+	resultType := reflect.SliceOf(reflect.TypeOf(data).Elem())
+	result := reflect.MakeSlice(resultType, 0, 5)
+	for rows.Next() {
+		if err := rows.Scan(params...); err != nil {
+			ctx.LogError("Error select data = %#v, err = %s", data, err.Error())
+			return nil, ERROR_DB_ERROR
+		}
+
+		result = reflect.Append(result, reflect.ValueOf(data).Elem())
+	}
+
+	return result.Interface(), nil
 }
 
 func (session oracleSession) SelectListByFields(ctx Context, data DataBaseObject, mapArgs map[string]interface{}) (any, Error) {
-	return nil, nil
+	query, params, err := GetSelectQuery(data)
+	if err != nil {
+		ctx.LogError("Error when get update data = %#v, err = %s", data, err.Error())
+		return nil, err
+	}
+
+	// Handle where params
+	var args map[string]any
+	var keys []string
+	if len(mapArgs) > 0 {
+		query += " WHERE "
+		args = map[string]any{}
+		keys = []string{}
+	}
+
+	count := 1
+	for key, value := range mapArgs {
+		if count == 1 {
+			query += fmt.Sprintf("%s = :%s ", key, key)
+		} else {
+			query += fmt.Sprintf("AND %s = :%s ", key, key)
+		}
+
+		args[key] = value
+		keys = append(keys, key)
+		count++
+	}
+
+	ctx.LogInfo("Select query = %v, args = %#v", query, args)
+	rows, errQuery := session.QueryContext(ctx, query, args)
+	if errQuery != nil {
+		ctx.LogError("Error select %#v = %#v, err = %s", keys, args, errQuery.Error())
+		return nil, ERROR_DB_ERROR
+	}
+
+	// Get list of struct
+	resultType := reflect.SliceOf(reflect.TypeOf(data).Elem())
+	result := reflect.MakeSlice(resultType, 0, 5)
+	for rows.Next() {
+		if err := rows.Scan(params...); err != nil {
+			ctx.LogError("Error select data = %#v, err = %s", data, err.Error())
+			return nil, ERROR_DB_ERROR
+		}
+
+		result = reflect.Append(result, reflect.ValueOf(data).Elem())
+
+	}
+
+	return result.Interface(), nil
 }
 
 func (session oracleSession) SelectListWithWhereQuery(ctx Context, data DataBaseObject, whereQuery string) (any, Error) {
-	return nil, nil
+	query, params, err := GetSelectQuery(data)
+	if err != nil {
+		ctx.LogError("Error when get update data = %#v, err = %s", data, err.Error())
+		return nil, err
+	}
+
+	query += " WHERE " + whereQuery
+
+	ctx.LogInfo("Select query = %s", query)
+	rows, errQuery := session.QueryContext(ctx, query)
+	if errQuery != nil {
+		ctx.LogError("Error select query: %s | err = %s", query, errQuery.Error())
+		return nil, ERROR_DB_ERROR
+	}
+
+	// Get list of struct
+	resultType := reflect.SliceOf(reflect.TypeOf(data).Elem())
+	result := reflect.MakeSlice(resultType, 0, 5)
+	for rows.Next() {
+		if err := rows.Scan(params...); err != nil {
+			ctx.LogError("Error select data = %#v, err = %s", data, err.Error())
+			return nil, ERROR_DB_ERROR
+		}
+
+		result = reflect.Append(result, reflect.ValueOf(data).Elem())
+	}
+
+	return result.Interface(), nil
 }
 
 func (session oracleSession) ListPagingTable(ctx Context, data DataBaseObject, limit int64, offset int64) (any, Error) {
-	return nil, nil
+	query, params, err := GetSelectQuery(data)
+	if err != nil {
+		ctx.LogError("Error when get update data = %#v, err = %s", data, err.Error())
+		return nil, err
+	}
+
+	query += " OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY"
+
+	args := map[string]any{
+		"offset": offset,
+		"limit":  limit,
+	}
+
+	ctx.LogInfo("Select query = %v", query)
+	rows, errQuery := session.QueryContext(ctx, query, args)
+	if errQuery != nil {
+		ctx.LogError("Error select table %s, err = %s", data.GetTableName(), errQuery.Error())
+		return nil, ERROR_DB_ERROR
+	}
+
+	// Get list of struct
+	resultType := reflect.SliceOf(reflect.TypeOf(data).Elem())
+	result := reflect.MakeSlice(resultType, 0, 5)
+	for rows.Next() {
+		if err := rows.Scan(params...); err != nil {
+			ctx.LogError("Error select data = %#v, err = %s", data, err.Error())
+			return nil, ERROR_DB_ERROR
+		}
+
+		result = reflect.Append(result, reflect.ValueOf(data).Elem())
+	}
+
+	return result.Interface(), nil
 }
 
 func (session oracleSession) SelectPagingListByFields(ctx Context, data DataBaseObject, mapArgs map[string]interface{}, limit int64, offset int64) (any, Error) {
-	return nil, nil
+	query, params, err := GetSelectQuery(data)
+	if err != nil {
+		ctx.LogError("Error when get update data = %#v, err = %s", data, err.Error())
+		return nil, err
+	}
+
+	// Handle where params
+	var args map[string]any
+	var keys []string
+	if len(mapArgs) > 0 {
+		query += " WHERE "
+		args = map[string]any{}
+		keys = []string{}
+	}
+
+	count := 1
+	for key, value := range mapArgs {
+		if count == 1 {
+			query += fmt.Sprintf("%s = $%d ", key, count)
+		} else {
+			query += fmt.Sprintf("AND %s = $%d ", key, count)
+		}
+
+		args[key] = value
+		keys = append(keys, key)
+		count++
+	}
+
+	query += " OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY"
+
+	args["offset"] = offset
+	args["limit"] = limit
+
+	ctx.LogInfo("Select query = %v, args = %#v", query, args)
+	rows, errQuery := session.QueryContext(ctx, query, args)
+	if errQuery != nil {
+		ctx.LogError("Error select %#v = %#v, err = %s", keys, args, errQuery.Error())
+		return nil, ERROR_DB_ERROR
+	}
+
+	// Get list of struct
+	resultType := reflect.SliceOf(reflect.TypeOf(data).Elem())
+	result := reflect.MakeSlice(resultType, 0, 5)
+	for rows.Next() {
+		if err := rows.Scan(params...); err != nil {
+			ctx.LogError("Error select data = %#v, err = %s", data, err.Error())
+			return nil, ERROR_DB_ERROR
+		}
+
+		result = reflect.Append(result, reflect.ValueOf(data).Elem())
+
+	}
+
+	return result.Interface(), nil
 }
 
 func (session oracleSession) CountRecordInTable(ctx Context, data DataBaseObject) (int64, Error) {
-	return 0, nil
+	query := fmt.Sprintf("SELECT COUNT(*) FROM %s", data.GetTableName())
+
+	row := session.QueryRowContext(ctx, query)
+
+	var count int64
+	err := row.Scan(&count)
+	if err != nil {
+		ctx.LogError("Error count record in table %s, err = %s", data.GetTableName(), err.Error())
+		return 0, NewError(ERROR_CODE_FROM_DATABASE, err.Error())
+	}
+	return count, nil
 }
 
 func (session oracleSession) CountRecordInTableWithWhere(ctx Context, data DataBaseObject, whereQuery string) (int64, Error) {
-	return 0, nil
+	query := fmt.Sprintf("SELECT COUNT(*) FROM %s WHERE %s", data.GetTableName(), whereQuery)
+	ctx.LogInfo("Count record in table with where query: %s", query)
+	row := session.QueryRowContext(ctx, query)
+
+	var count int64
+	err := row.Scan(&count)
+	if err != nil {
+		ctx.LogError("Error count record in table %s, err = %s", data.GetTableName(), err.Error())
+		return 0, NewError(ERROR_CODE_FROM_DATABASE, err.Error())
+	}
+	return count, nil
 }
---
+++ query_oracle.go
@@ -0,0 +1,280 @@
+package core
+
+import (
+	"fmt"
+	"reflect"
+)
+
+/*
+* Get insert query for oracle: generate an insert query from a model
+* @params: model DataBaseObject
+* @return: string, map[string]any, Error
+ */
+func GetInsertQueryForOracle[T DataBaseObject](model T) (string, map[string]any, Error) {
+	t, err := getTypeOfPointer(model)
+	if err != nil {
+		return BLANK, nil, err
+	}
+	v := reflect.ValueOf(model).Elem()
+
+	// Generate insert query
+	tableName := model.GetTableName()
+	fields := BLANK
+	questionString := BLANK
+	args := map[string]any{}
+	count := 1
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("db")
+
+		if tag == BLANK {
+			continue
+		}
+
+		if i != t.NumField()-1 {
+			fields += tag + ","
+			questionString += fmt.Sprintf(":%s,", tag)
+		} else {
+			fields += tag
+			questionString += fmt.Sprintf(":%s", tag)
+		}
+
+		count++
+		args[tag] = v.Field(i).Interface()
+	}
+
+	if len(fields) == 0 {
+		return BLANK, nil, ERROR_MODEL_HAVE_NO_FIELD
+	}
+
+	if fields[len(fields)-1:] == "," {
+		fields = fields[:len(fields)-1]
+		questionString = questionString[:len(questionString)-1]
+	}
+
+	query := fmt.Sprintf("INSERT INTO %s(%s) VALUES(%s)", tableName, fields, questionString)
+
+	return query, args, nil
+}
+
+/*
+* Get insert query for oracle: generate an insert query from a model without insert primary key
+* @params: model DataBaseObject
+* @return: string, map[string]any, Error
+ */
+func GetInsertQueryWithoutPrimaryKeyForOracle[T DataBaseObject](model T) (string, map[string]any, any, Error) {
+	t, err := getTypeOfPointer(model)
+	if err != nil {
+		return BLANK, nil, nil, err
+	}
+	v := reflect.ValueOf(model).Elem()
+
+	// Generate insert query
+
+	tableName := model.GetTableName()
+	// Primary key
+	primaryKeys, numPrimaryKeys := splitPrimaryKey(model)
+	if numPrimaryKeys > 1 {
+		return BLANK, nil, nil, ERROR_NO_SUPPORT_FOR_MANY_PRIMARY_KEYS
+	} else if numPrimaryKeys == 0 {
+		return BLANK, nil, nil, ERROR_NOT_FOUND_PRIMARY_KEY
+	}
+
+	var primaryKeyAddress interface{}
+	foundPrimaryKey := false
+
+	fields := BLANK
+	questionString := BLANK
+	args := map[string]any{}
+	count := 1
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("db")
+
+		if tag == BLANK {
+			continue
+		}
+
+		if tag == primaryKeys[0] {
+			primaryKeyAddress = v.Field(i).Addr().Interface()
+			foundPrimaryKey = true
+			continue
+		}
+
+		if i != t.NumField()-1 {
+			fields += tag + ","
+			questionString += fmt.Sprintf(":%s,", tag)
+		} else {
+			fields += tag
+			questionString += fmt.Sprintf(":%s", tag)
+		}
+
+		count++
+		args[tag] = v.Field(i).Interface()
+	}
+
+	if len(fields) == 0 {
+		return BLANK, nil, primaryKeyAddress, ERROR_MODEL_HAVE_NO_FIELD
+	}
+
+	if !foundPrimaryKey {
+		return BLANK, nil, primaryKeyAddress, ERROR_NOT_FOUND_PRIMARY_KEY
+	}
+
+	if fields[len(fields)-1:] == "," {
+		fields = fields[:len(fields)-1]
+		questionString = questionString[:len(questionString)-1]
+	}
+
+	query := fmt.Sprintf("INSERT INTO %s(%s) VALUES(%s) RETURNING %s INTO :%s", tableName, fields, questionString, primaryKeys[0], primaryKeys[0])
+
+	// Append primary key to args
+	args[primaryKeys[0]] = primaryKeyAddress
+
+	return query, args, primaryKeyAddress, nil
+}
+
+/*
+* Get delete query for oracle: generate a delete query from a model
+* @params: model DataBaseObject
+* @return: string, []any, error
+ */
+func GetDeleteQueryForOracle[T DataBaseObject](model T) (string, map[string]any, Error) {
+	// Check model is pointer of struct
+	_, err := getTypeOfPointer(model)
+	if err != nil {
+		return BLANK, nil, err
+	}
+
+	tableName := model.GetTableName()
+	pkValues, found := searchMapPrimaryKey(model)
+	if !found {
+		return BLANK, nil, ERROR_NOT_FOUND_PRIMARY_KEY
+	}
+
+	primaryKeys, number := splitPrimaryKey(model)
+	if number != len(pkValues) {
+		return BLANK, nil, ERROR_NOT_FOUND_PRIMARY_KEY
+	}
+
+	query := fmt.Sprintf("DELETE FROM %s", tableName)
+	for i, key := range primaryKeys {
+		if i == 0 {
+			query += fmt.Sprintf(" WHERE %s = :%s", key, key)
+		} else {
+			query += fmt.Sprintf(" AND %s = :%s", key, key)
+		}
+	}
+
+	return query, pkValues, nil
+}
+
+/*
+* Get update query: generate an update query from a model
+* @params: model DataBaseObject
+* @return: string, map[string]any, Error
+ */
+func GetUpdateQueryForOracle[T DataBaseObject](model T) (string, map[string]any, Error) {
+	t, err := getTypeOfPointer(model)
+	if err != nil {
+		return BLANK, nil, err
+	}
+	v := reflect.ValueOf(model).Elem()
+
+	tableName := model.GetTableName()
+	primaryKeys, numPrimaryKeys := splitPrimaryKey(model)
+	if numPrimaryKeys == 0 {
+		return BLANK, nil, ERROR_NOT_FOUND_PRIMARY_KEY
+	}
+	primaryValues := map[string]any{}
+
+	var setString string
+	args := map[string]any{}
+	count := 1
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("db")
+
+		if tag == BLANK {
+			continue
+		}
+
+		isPrimaryKey := false
+		for _, key := range primaryKeys {
+			if tag == key {
+				isPrimaryKey = true
+				primaryValues[tag] = v.Field(i).Interface()
+			}
+		}
+
+		if isPrimaryKey {
+			continue
+		}
+
+		value := v.Field(i).Interface()
+
+		if i != t.NumField()-1 {
+			setString += fmt.Sprintf("%s = :%s, ", tag, tag)
+		} else {
+			setString += fmt.Sprintf("%s = :%s", tag, tag)
+		}
+		count++
+		args[tag] = value
+	}
+
+	// Check argument and primary key
+	if len(args) == 0 {
+		return BLANK, nil, ERROR_MODEL_HAVE_NO_FIELD
+	}
+	if len(primaryValues) == 0 || len(primaryKeys) != len(primaryValues) {
+		return BLANK, nil, ERROR_NOT_FOUND_PRIMARY_KEY
+	}
+
+	if setString[len(setString)-2:] == ", " {
+		setString = setString[:len(setString)-2]
+	}
+
+	query := fmt.Sprintf("UPDATE %s SET %s", tableName, setString)
+	for i, key := range primaryKeys {
+		if i == 0 {
+			query += fmt.Sprintf(" WHERE %s = :%s", key, key)
+		} else {
+			query += fmt.Sprintf(" AND %s = :%s", key, key)
+		}
+
+		args[key] = primaryValues[key]
+	}
+
+	return query, args, nil
+}
+
+/*
+* searchMapPrimaryKey: search primary key in model
+* @params: data DataBaseObject
+* @return: map[string]any, bool
+ */
+func searchMapPrimaryKey(data DataBaseObject) (map[string]any, bool) {
+	t, _ := getTypeOfPointer(data)
+	found := false
+	idValues := map[string]any{}
+	primaryKeys, numPrimaryKeys := splitPrimaryKey(data)
+	if numPrimaryKeys == 0 {
+		return nil, false
+	}
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("db")
+		for _, key := range primaryKeys {
+			if tag == key {
+				idValues[key] = reflect.ValueOf(data).Elem().FieldByIndex(field.Index).Interface()
+				break
+			}
+		}
+	}
+
+	if len(idValues) == numPrimaryKeys {
+		found = true
+	}
+	return idValues, found
+}
