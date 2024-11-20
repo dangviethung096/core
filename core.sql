ed0b0697e25774809f630af6dff694817c944ff8
Đặng Việt Hưng <dangviethung096@gmail.com>
2024-11-20 14:02:47 +0700

    update task

--- core.sql
+++ core.sql
@@ -1,35 +1,37 @@
 DROP TABLE IF EXISTS scheduler_tasks;
 
-CREATE TABLE scheduler_tasks (
-    id SERIAL PRIMARY KEY,
-    queue_name TEXT,
-    data BYTEA,
-    done BOOLEAN,
-    loop_index BIGINT,
-    loop_count BIGINT,
+CREATE TABLE scheduler_tasks(
+    id serial PRIMARY KEY,
+    task_name text,
+    queue_name text,
+    data bytea,
+    done boolean,
+    loop_index bigint,
+    loop_count bigint,
     next BIGINT,
-    next_time TEXT,
-    interval BIGINT,
-    start_time TEXT,
-    source TEXT
+    next_time text,
+    interval bigint,
+    start_time text,
+    source text
 );
 
 DROP TABLE IF EXISTS scheduler_todo;
 
-CREATE TABLE scheduler_todo (
-    id BIGSERIAL PRIMARY KEY,
-    task_id INT,
-    bucket BIGINT,
-    next_time TEXT,
-    source TEXT
+CREATE TABLE scheduler_todo(
+    id bigserial PRIMARY KEY,
+    task_id int,
+    bucket bigint,
+    next_time text,
+    source text
 );
 
 DROP TABLE IF EXISTS scheduler_done;
 
-CREATE TABLE scheduler_done (
-    id BIGSERIAL PRIMARY KEY,
-    bucket BIGINT,
-    task_id BIGINT,
-    operation_time TEXT,
-    status TEXT
+CREATE TABLE scheduler_done(
+    id bigserial PRIMARY KEY,
+    bucket bigint,
+    task_id bigint,
+    operation_time text,
+    status text
 );
+
--- task.go
+++ task.go
@@ -2,11 +2,13 @@ package core
 
 import (
 	"database/sql"
+	"fmt"
 	"math"
 	"time"
 )
 
 type StartTaskRequest struct {
+	TaskName  string `validate:"required"`
 	QueueName string `validate:"required"`
 	Data      []byte
 	Time      time.Time `validate:"required"`
@@ -58,10 +60,11 @@ func StartTask(ctx Context, request *StartTaskRequest) Error {
 	var startTime string
 	var source string
 	var loopCount, interval int64
-	row := DBSession().QueryRowContext(ctx, "SELECT id, start_time, loop_count, interval, source FROM scheduler_tasks WHERE queue_name = $1", request.QueueName)
-	err = row.Scan(&id, &startTime, &loopCount, &interval, &source)
+	var queueName string
+	row := DBSession().QueryRowContext(ctx, "SELECT id, start_time, loop_count, interval, source, queue_name FROM scheduler_tasks WHERE task_name = $1", request.TaskName)
+	err = row.Scan(&id, &startTime, &loopCount, &interval, &source, &queueName)
 	if err == nil {
-		if startTime == request.Time.Format(time.RFC3339) && loopCount == int64(request.Loop) && interval == request.Interval && source == Config.Server.Name {
+		if startTime == request.Time.Format(time.RFC3339) && loopCount == int64(request.Loop) && interval == request.Interval && source == Config.Server.Name && queueName == request.QueueName {
 			ctx.LogInfo("Task %#v already exist in db with id = %s", *request, id)
 			return ERROR_TASK_ALREADY_EXISTED
 		}
@@ -79,8 +82,8 @@ func StartTask(ctx Context, request *StartTaskRequest) Error {
 
 	ctx.LogInfo("Insert new task: %d, startTime: %s, loopCount: %d, interval: %d", taskId, request.Time.String(), request.Loop, request.Interval)
 	row = tx.QueryRowContext(ctx,
-		"INSERT INTO scheduler_tasks(queue_name, data, done, loop_index, loop_count, next, interval, start_time, source, next_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id;",
-		request.QueueName, request.Data, false, loopIndex, request.Loop, nextTime.Unix(), request.Interval, request.Time.Format(time.RFC3339), Config.Server.Name, nextTime.Format(time.RFC3339))
+		"INSERT INTO scheduler_tasks(task_name, queue_name, data, done, loop_index, loop_count, next, interval, start_time, source, next_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id;",
+		request.TaskName, request.QueueName, request.Data, false, loopIndex, request.Loop, nextTime.Unix(), request.Interval, request.Time.Format(time.RFC3339), Config.Server.Name, nextTime.Format(time.RFC3339))
 	if err := row.Scan(&taskId); err != nil {
 		ctx.LogError("Insert task fail: %v, err = %s", *request, err.Error())
 		return ERROR_ADD_TASK_SYSTEM_FAIL
@@ -136,4 +139,23 @@ func StopTask(ctx Context, request *StopTaskRequest) Error {
 
 func TriggerTask(ctx Context, taskName string, taskData []byte) Error {
 	return pushTaskToQueue(ctx, taskName, taskData)
+}
+
+func StartOneTimeTask(ctx Context, queueName string, startTime time.Time, taskData []byte) (string, Error) {
+	taskName := fmt.Sprintf("onetime_%s_%s", queueName, startTime.Format(time.RFC3339))
+
+	now := time.Now()
+	if startTime.Before(now) {
+		ctx.LogError("StartOneTimeTask: startTime is before now: %s", startTime.Format(time.RFC3339))
+		return BLANK, ERROR_TASK_REQUEST_INVALID
+	}
+
+	return taskName, StartTask(ctx, &StartTaskRequest{
+		TaskName:  taskName,
+		QueueName: queueName,
+		Time:      startTime,
+		Data:      taskData,
+		Interval:  0,
+		Loop:      0,
+	})
 }
