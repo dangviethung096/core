aa323d3522f3575c264231d2d04dcabc118895d4
Dang Viet Hung <dangviethung096@gmail.com>
2024-12-24 10:30:03 +0700

    reformat

--- core.sql
+++ core.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS scheduler_tasks;
 
-CREATE TABLE scheduler_tasks(
+CREATE TABLE scheduler_tasks (
     id serial PRIMARY KEY,
     task_name text,
     queue_name text,
@@ -17,7 +17,7 @@ CREATE TABLE scheduler_tasks(
 
 DROP TABLE IF EXISTS scheduler_todo;
 
-CREATE TABLE scheduler_todo(
+CREATE TABLE scheduler_todo (
     id bigserial PRIMARY KEY,
     task_id int,
     bucket bigint,
@@ -27,11 +27,10 @@ CREATE TABLE scheduler_todo(
 
 DROP TABLE IF EXISTS scheduler_done;
 
-CREATE TABLE scheduler_done(
+CREATE TABLE scheduler_done (
     id bigserial PRIMARY KEY,
     bucket bigint,
     task_id bigint,
     operation_time text,
     status text
 );
-
